d1ca64601dee21a858f60df74546148ba92b9698
verdxhz <143743489+verdxhz@users.noreply.github.com>
2023-09-12 22:15:19 +0200

    Add files via upload

---
+++ progetto/griglia/Blocco.java
@@ -0,0 +1,20 @@
+package so.ing.progetto.griglia;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+
+public class Blocco implements Serializable
+{
+    private static final long serialVersionUID=1L;
+    protected LinkedList<Casella> caselleBlocco= new LinkedList<>();
+    final protected int risultato;
+    final protected Character operazione;
+    public Blocco(LinkedList<Casella> caselle, int ris, Character op)
+    {
+        risultato=ris;
+        operazione=op;
+        for(Casella c: caselle)
+            caselleBlocco.add(c);
+
+    }
+}
